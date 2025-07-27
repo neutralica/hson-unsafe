ef52f67fea7a7db3c9bd35862e229a736780eac1
Phillip Hanson <hansonpw@gmail.com>
2025-07-27 03:02:57 -0500

    npm run build

--- src/utils/coerce-string.utils.hson.ts
+++ src/utils/coerce-string.utils.hson.ts
@@ -14,6 +14,7 @@ export function coerce(value: string): BasicValue {
         return '';
     }
 
+    
     /* 1. check for a quoted string literal first */
     if (trimmed.startsWith('"') && trimmed.endsWith('"')) {
         try {
