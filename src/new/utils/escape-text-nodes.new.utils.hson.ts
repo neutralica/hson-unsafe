bbeb4a30b83069c8999555569ee003f1fc9a583c
Phillip Hanson <hansonpw@gmail.com>
2025-09-05 10:28:53 -0500

    text node escaper fine tuned to handle <> as text

--- src/new/utils/escape-text-nodes.new.utils.hson.ts
+++ src/new/utils/escape-text-nodes.new.utils.hson.ts
@@ -8,51 +8,36 @@
  */
 export function escape_text_nodes(input: string): string {
   let out = "";
-  let i = 0;
-  const n = input.length;
+  let i = 0, n = input.length;
   let inTag = false;
   let quote: '"' | "'" | null = null;
 
-  // Escape bare ampersands that aren't entities
   const escapeText = (chunk: string) =>
     chunk
       .replace(/&(?!#\d+;|#x[0-9A-Fa-f]+;|[a-zA-Z][\w.-]*;)/g, "&amp;")
       .replace(/</g, "&lt;")
       .replace(/>/g, "&gt;");
 
+  // NEW: XML NameStartChar (very simplified) + specials
+  const isTagStarter = (c: string | undefined) =>
+    !!c && (c === ":" || c === "_" || /[A-Za-z]/.test(c) || c === "!" || c === "?" || c === "/");
+
   while (i < n) {
     if (!inTag) {
-      // accumulate text until next "<"
-      let j = i;
-      while (j < n && input[j] !== "<") j++;
-      if (j > i) out += escapeText(input.slice(i, j));
-      if (j < n && input[j] === "<") {
-        inTag = true;
-        out += "<";
-        i = j + 1;
-      } else {
-        i = j;
-      }
-    } else {
-      // inside a tag; copy verbatim until the matching ">"
       let j = i;
       while (j < n) {
-        const c = input[j] as string;
-        if (quote) {
-          if (c === quote) quote = null;
-          j++;
-          continue;
-        }
-        if (c === '"' || c === "'") {
-          quote = c as '"' | "'";
-          j++;
-          continue;
-        }
-        if (c === ">") {
-          j++;
-          inTag = false;
-          break;
-        }
+        if (input[j] === "<" && isTagStarter(input[j + 1])) break; // CHANGED
+        j++;
+      }
+      if (j > i) out += escapeText(input.slice(i, j));
+      if (j < n) { inTag = true; out += "<"; i = j + 1; } else { i = j; }
+    } else {
+      let j = i;
+      while (j < n) {
+        const c = input[j]!;
+        if (quote) { if (c === quote) quote = null; j++; continue; }
+        if (c === '"' || c === "'") { quote = c as '"' | "'"; j++; continue; }
+        if (c === ">") { j++; inTag = false; break; }
         j++;
       }
       out += input.slice(i, j);
